b9530b5304e18377bd2a079a15d2c88c7ad7babd
macbook <houssi27@hotmail.com>
2022-11-20 18:11:53 +0100

    [First Commit]

---
+++ modules/gestion_hoteliere/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Gestion Hotelière',
+    'version': '1.0',
+    "author": "El Houcine BRAHIM",
+    "category": "Hotel Management",
+    'description': """Module de base pour la gestion d'hotel""",
+    'depends': [],
+    'data': [
+        # security
+
+        # data
+
+        # views && actions
+
+    ],
+    'qweb': [],
+    'demo': [],
+    'test': [],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+}
+
